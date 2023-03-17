7caf82d962f26feef475ef93d7e0e108bca9e8cb
Gisselle Garcia <gissellegar46@gmail.com>
2023-03-16 23:23:05 -0700

    Change margins

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import Events from './components/events';
 function App() {
   return (
     <div className="App">
+      <div id="h1" >
       <h1>Techtonica 2023 events</h1>
+      </div>
     <Events />
     </div>
   );
